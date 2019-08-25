8f83323f40e8233b8ccdb79680482db91943177f
xueba609 <2733089907@qq.com>
2019-08-25 19:44:42 +0800

    SSM项目-人力资源系统

---
+++ SSM_Farmer2/src/main/java/com/hrms/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.hrms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value = "/hrms")
+public class MainController {
+
+    @RequestMapping(value = "companyinfo", method = RequestMethod.GET)
+    public String company_info(){
+        return "/companyInfo";
+    }
+}
---
+++ SSM_Farmer2/src/main/webapp/WEB-INF/jsp/commom/foot.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Foot Page</title>
+</head>
+<body>
+<div class="hrms_footer" style="text-align: center;">
+    <address>
+        <strong>Copyright&copy;草滩国际金融公司.Inc</strong><br>
+        草滩国际大厦2-222<br>
+        <abbr title="Phone">P:</abbr> (123) 456-88888888
+    </address>
+</div><!-- /.hrms_footer -->
+</body>
+</html>
